2670756476a5dfb17adda972799b102841a864eb
Ollie <oliver.lawrence169@gmail.com>
2024-05-05 15:55:24 +0100

    Created Lexer

---
+++ Lexer.cs
@@ -0,0 +1,148 @@
+namespace LexerNS;
+
+enum TokenType {
+    LBRA, // {
+    RBRA, // }
+    LARR, // [
+    RARR, // ]
+    SC, // :
+    COMMA, // ,
+
+    NUM, // number
+    STR, // "string"
+    TRUE, // true
+    FALSE, // false
+    NULL, // null
+
+    EOF, // end of file
+
+    
+}
+
+struct Token {
+    public TokenType Type { get; private set; }
+    public string Lexeme { get; private set; }
+
+    public Token(TokenType type, string lexeme)  { 
+        this.Type = type; 
+        this.Lexeme  = lexeme; 
+    }
+
+    public override string ToString() {
+        return $"Token (Type: {this.Type}, Lexeme: '{this.Lexeme}')";
+    }
+}
+
+class Lexer {
+    private StreamReader sr;
+
+    public Lexer(string fileName) {
+        this.sr = new StreamReader(fileName);
+    }
+
+    public Token GetTok() {
+        if (sr.Peek() < 0) {
+            return new Token(TokenType.EOF, "");
+        }
+
+        char c = (char) sr.Read();
+
+        return c switch {
+            ' '  => GetTok(),
+            '\n' => GetTok(),
+            '\r' => GetTok(),
+            '\t' => GetTok(),
+            '{'  => new Token(TokenType.LBRA, "{"),
+            '}'  => new Token(TokenType.RBRA, "}"),
+            '['  => new Token(TokenType.LARR, "["),
+            ']'  => new Token(TokenType.RARR, "]"),
+            ':'  => new Token(TokenType.SC, ":"),
+            ','  => new Token(TokenType.COMMA, ","),
+            '"'  => ReadStr(),
+            't'  => ReadTrue(),
+            'f'  => ReadFalse(),
+            'n'  => ReadNull(),
+            
+            _    => Char.IsDigit(c) ? ReadNum(c) : throw new FormatException($"Error in Lexer.GetTok: invalid input character: {c}.")
+        };
+    }
+
+    private Token ReadStr() {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        while (sr.Peek() >= 0) {
+            char c = (char) sr.Read();
+
+            if (c == '"') {
+                return new Token(TokenType.STR, sb.ToString());
+            }
+
+            sb.Append(c);
+        }
+
+        throw new FormatException("Error in Lexer.ReadStr: reached EOF before string was closed\n");
+    }
+
+    private Token ReadNum(char start) {
+        if (!Char.IsDigit(start)) {
+            throw new FormatException("Error in Lexer.ReadNum: invalid number start");
+        }
+
+        char lookahead;
+        char c;
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        sb.Append(start);
+        while (sr.Peek() >= 0) {
+            lookahead = (char) sr.Peek();
+
+            if (Char.IsDigit(lookahead)) {
+                c = (char) sr.Read();
+                sb.Append(c);
+            }
+            else {
+                return new Token(TokenType.NUM, sb.ToString());
+            }
+        }
+
+        throw new FormatException("Error in Lexer.ReadNum: reached EOF before num ended\n");
+    }
+
+    private Token ReadTrue() {
+        char[] next = new char[3];
+        if (sr.Read(next, 0, 3) != 3) {
+            throw new FormatException("Error in Lexer.ReadTrue: reached EOF while attempting to read bool\n");
+        }
+
+        if (new string(next) == "rue") {
+            return new Token(TokenType.TRUE, "true");
+        }
+
+        throw new FormatException("Error in Lexer.ReadTrue: couldnt match bool\n");
+    }
+
+    private Token ReadFalse() {
+        char[] next = new char[4];
+        if (sr.Read(next, 0, 4) != 4) {
+            throw new FormatException("Error in Lexer.ReadFalse: reached EOF while attempting to read bool\n");
+        }
+
+        if (new string(next) == "alse") {
+            return new Token(TokenType.FALSE, "false");
+        }
+
+        throw new FormatException("Error in Lexer.ReadFalse: couldnt match bool\n");
+    }
+
+    private Token ReadNull() {
+        char[] next = new char[3];
+        if (sr.Read(next, 0, 3) != 3) {
+            throw new FormatException("Error in Lexer.ReadNull: reached EOF while attempting to read null\n");
+        }
+
+        if (new string(next) == "ull") {
+            return new Token(TokenType.NULL, "null");
+        }
+
+        throw new FormatException("Error in Lexer.ReadNull: couldnt match null\n");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LexerNS;
+
+
+string fileName = "../../../test.json";
+Lexer l = new Lexer(fileName);
+Console.WriteLine($"Lexing {fileName}");
+Token cur;
+
+while ((cur = l.GetTok()).Type != TokenType.EOF) {
+    Console.WriteLine(cur);
+}
+
+Console.WriteLine("Reached EOF and finished Lexing");
